a55a8227aa36f2d4d655bc7c8efe5d9817b667ce
YOUR NAME <YOUR@EMAIL.com>
2019-10-22 15:07:52 +0900

    add Draw Function

--- app/src/main/java/com/oshimamasara/ai_abc01/MainActivity.java
+++ app/src/main/java/com/oshimamasara/ai_abc01/MainActivity.java
@@ -3,12 +3,22 @@ package com.oshimamasara.ai_abc01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PaintView paintView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        paintView = (PaintView) findViewById(R.id.canvas);
     }
+
+    public void clearCanvas(View view) {
+        paintView.clearCanvas();
+    }
+
 }
